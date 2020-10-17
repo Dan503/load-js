4274e6060af9cffa3a41529823904d967825fa7b
Daniel Tonon <dan503.npm@gmail.com>
2020-10-17 13:55:16 +1100

    If script has already previously loaded, callback runs immediately

--- index.ts
+++ index.ts
@@ -1,29 +1,38 @@
 const alreadyCalledSources: Array<string> = []
 
 interface Callbacks {
-	[key: string]: Array<Function>
+	[key: string]: {
+		hasLoaded: boolean
+		callbacks: Array<Function>
+	}
 }
 
 const awaitingCallbacks: Callbacks = {}
 
 const addCallback = (src: string, callback: Function): void => {
-	if (awaitingCallbacks[src]) {
-		awaitingCallbacks[src].push(callback)
+	if (awaitingCallbacks[src].hasLoaded) {
+		// If the script has already been previously loaded, just run the callback immediately
+		callback()
 	} else {
-		awaitingCallbacks[src] = [callback]
+		if (awaitingCallbacks[src].callbacks) {
+			awaitingCallbacks[src].callbacks.push(callback)
+		} else {
+			awaitingCallbacks[src].callbacks = [callback]
+		}
 	}
 }
 
 export default function loadJS(src: string, callback: () => void): void {
+	awaitingCallbacks[src] = awaitingCallbacks[src] || {}
 	if (alreadyCalledSources.indexOf(src) < 0) {
 		alreadyCalledSources.push(src)
 		const script = document.createElement('script')
 		script.src = src
 		script.onload = (): any => {
 			addCallback(src, callback)
-
+			awaitingCallbacks[src].hasLoaded = true
 			for (const key in awaitingCallbacks) {
-				awaitingCallbacks[key].forEach(cb => cb())
+				awaitingCallbacks[key].callbacks.forEach(cb => cb())
 			}
 		}
 		document.head.appendChild(script)
