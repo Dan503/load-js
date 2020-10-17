a1eb78e711b5a9dc6bd79099b2802fc16d0152f8
Daniel Tonon <dan503.npm@gmail.com>
2020-10-17 15:23:42 +1100

    Using Cypress Promises to ensure tests handle asyncronicity correctly

--- cypress/integration/tests.test.ts
+++ cypress/integration/tests.test.ts
@@ -10,60 +10,93 @@ describe('load JS tests', () => {
 	it('Expect to not exist then to exist', () => {
 		cy.visit('./cypress/fixtures/test.html').then(() => {
 			expect(window.moment).to.not.exist
-			loadJS(jsLibrarySrc, () => {
-				expect(window.moment).to.exist
+			return new Cypress.Promise((resolve) => {
+				loadJS(jsLibrarySrc, () => {
+					expect(window.moment).to.exist
+					resolve()
+				})
 			})
 		})
 	})
 
 	it('Expect to only create a single script element per JS source', () => {
 		cy.visit('./cypress/fixtures/test.html').then(() => {
-			loadJS(jsLibrarySrc, () => {
-				const $script = document.querySelectorAll(`script.load-js-script[src="${jsLibrarySrc}"]`)
-				expect($script.length).to.equal(1)
-			})
-			loadJS(altJsLibrarySrc, () => {
-				const $script = document.querySelectorAll(`script.load-js-script[src="${altJsLibrarySrc}"]`)
-				expect($script.length).to.equal(1)
-			})
-			loadJS(jsLibrarySrc, () => {
-				const $script = document.querySelectorAll('script.load-js-script')
-				expect($script.length).to.equal(2)
-			})
+			return Cypress.Promise.all([
+				new Cypress.Promise((resolve) => {
+					loadJS(jsLibrarySrc, () => {
+						const $script = document.querySelectorAll(`script.load-js-script[src="${jsLibrarySrc}"]`)
+						expect($script.length).to.equal(1)
+						resolve()
+					})
+				}),
+				new Cypress.Promise((resolve) => {
+					loadJS(altJsLibrarySrc, () => {
+						const $script = document.querySelectorAll(`script.load-js-script[src="${altJsLibrarySrc}"]`)
+						expect($script.length).to.equal(1)
+						resolve()
+					})
+				}),
+				new Cypress.Promise((resolve) => {
+					loadJS(jsLibrarySrc, () => {
+						const $script = document.querySelectorAll('script.load-js-script')
+						expect($script.length).to.equal(2)
+						resolve()
+					})
+				}),
+			])
 		})
 	})
 
 	it('Expect to run all callbacks', () => {
 		let calls = 0;
 		cy.visit('./cypress/fixtures/test.html').then(() => {
-			loadJS(jsLibrarySrc, () => {
-				calls++
-				expect(calls).to.equal(1)
-			})
-			loadJS(jsLibrarySrc, () => {
-				calls++
-				expect(calls).to.equal(2)
-			})
-			loadJS(jsLibrarySrc, () => {
-				calls++
-				expect(calls).to.equal(3)
-			})
+			return Cypress.Promise.all([
+				new Cypress.Promise((resolve) => {
+					loadJS(jsLibrarySrc, () => {
+						calls++
+						expect(calls).to.equal(1)
+						resolve()
+					})
+				}),
+				new Cypress.Promise((resolve) => {
+					loadJS(jsLibrarySrc, () => {
+						calls++
+						expect(calls).to.equal(2)
+						resolve()
+					})
+				}),
+				new Cypress.Promise((resolve) => {
+					loadJS(jsLibrarySrc, () => {
+						calls++
+						expect(calls).to.equal(3)
+						resolve()
+					})
+				}),
+			])
 		})
 	})
 
-	it('Expect to run callback immediately if script previously loaded', () => {
+	it('Expect to run callback immediately if the script has already been loaded', () => {
 		let calls = 0;
 		cy.visit('./cypress/fixtures/test.html').then(() => {
-			loadJS(jsLibrarySrc, () => {
-				calls++
-				expect(calls).to.equal(1)
-			})
-			setTimeout(() => {
-				loadJS(jsLibrarySrc, () => {
-					calls++
-					expect(calls).to.equal(2)
+			return Cypress.Promise.all([
+				new Cypress.Promise((resolve) => {
+					loadJS(jsLibrarySrc, () => {
+						calls++
+						expect(calls).to.equal(1)
+						resolve()
+					})
+				}),
+				cy.wait(1000).then(() => {
+					new Cypress.Promise((resolve) => {
+						loadJS(jsLibrarySrc, () => {
+							calls++
+							expect(calls).to.equal(2)
+							resolve()
+						})
+					})
 				})
-			}, 1000)
+			])
 		})
 	})
 })
