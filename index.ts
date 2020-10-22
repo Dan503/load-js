ffd0f4e1d758a3ff34d6a4c45eeb04c92c05a824
Daniel Tonon <dan503.npm@gmail.com>
2020-10-22 23:37:39 +1100

    Fix for "{} not asignable to { hasLoaded: boolean; callbacks: Function[] }" TS error
    Also improved some of the variable names

--- index.ts
+++ index.ts
@@ -1,43 +1,53 @@
-const alreadyCalledSources: Array<string> = []
-
-interface Callbacks {
-	[key: string]: {
-		hasLoaded: boolean
-		callbacks: Array<Function>
-	}
+interface AllScripts {
+	[scriptSource: string]: undefined | Script
 }
 
-const awaitingCallbacks: Callbacks = {}
+interface Script {
+	hasLoaded: boolean
+	callbacks: Array<Function>
+}
+
+const alreadyCalledScripts: Array<string> = []
+
+const allScripts: AllScripts = {}
+const defaultScript: Script = { hasLoaded: false, callbacks: [] }
 
 const addCallback = (src: string, callback: Function): void => {
-	if (awaitingCallbacks[src].hasLoaded) {
+	const script = allScripts[src]
+	if (!script) return
+
+	if (script.hasLoaded) {
 		// If the script has already been previously loaded, just run the callback immediately
 		callback()
 	} else {
-		if (awaitingCallbacks[src].callbacks) {
-			awaitingCallbacks[src].callbacks.push(callback)
+		if (script.callbacks.length > 0) {
+			script.callbacks.push(callback)
 		} else {
-			awaitingCallbacks[src].callbacks = [callback]
+			script.callbacks = [callback]
 		}
 	}
 }
 
 export default function loadJS(src: string, callback: () => void): void {
-	awaitingCallbacks[src] = awaitingCallbacks[src] || {}
-	if (alreadyCalledSources.indexOf(src) < 0) {
-		alreadyCalledSources.push(src)
-		const script = document.createElement('script')
-		script.setAttribute('class', 'load-js-script')
-		script.src = src
-		script.onload = (): any => {
+	const script = allScripts[src] || defaultScript
+	allScripts[src] = script
+	if (alreadyCalledScripts.indexOf(src) < 0) {
+		alreadyCalledScripts.push(src)
+		const $scriptElem = document.createElement('script')
+		$scriptElem.setAttribute('class', 'load-js-script')
+		$scriptElem.src = src
+		$scriptElem.onload = (): void => {
 			addCallback(src, callback)
-			awaitingCallbacks[src].hasLoaded = true
-			for (const key in awaitingCallbacks) {
-				awaitingCallbacks[key].callbacks.forEach(cb => cb())
+			const updatedScript = allScripts[src]
+			if (updatedScript) {
+				updatedScript.hasLoaded = true
+			}
+			for (const thisSource in allScripts) {
+				const thisScript = allScripts[thisSource] || defaultScript
+				thisScript.callbacks.forEach(cb => cb())
 			}
 		}
-		document.head.appendChild(script)
-		console.log(document.head)
+		document.head.appendChild($scriptElem)
 	} else {
 		addCallback(src, callback)
 	}
