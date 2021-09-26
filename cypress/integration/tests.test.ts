0589022e375fc3da460fafbbc565460b80b12945
Daniel Tonon <dan503.npm@gmail.com>
2021-09-26 19:03:04 +1000

    WIP: Current tests

--- cypress/integration/tests.test.ts
+++ cypress/integration/tests.test.ts
@@ -8,22 +8,36 @@ const altJsLibrarySrc = 'https://cdnjs.cloudflare.com/ajax/libs/d3/6.2.0/d3.js'
 
 const testHtmlFile = './cypress/fixtures/test.html'
 
+const Window = (window as any)
+
+const reset = () => {
+	Window.moment = undefined
+	Window.d3 = undefined
+}
+
 describe('load JS tests', () => {
 	it('Expect to not exist then to exist', () => {
 		cy.visit(testHtmlFile).then(() => {
-			expect(window.moment).to.not.exist
+			expect(Window.moment).to.not.exist
 			return new Cypress.Promise((resolve) => {
 				loadJS(jsLibrarySrc, () => {
-					expect(window.moment).to.exist
+					expect(Window.moment).to.exist
 					resolve()
 				})
-			})
+			}).then(reset)
 		})
 	})
 
 	it('Expect to only create a single script element per JS source', () => {
 		cy.visit(testHtmlFile).then(() => {
 			return Cypress.Promise.all([
+				new Cypress.Promise((resolve) => {
+					loadJS(jsLibrarySrc, () => {
+						const $script = document.querySelectorAll(`script.load-js-script[src="${jsLibrarySrc}"]`)
+						expect($script.length).to.equal(1)
+						resolve()
+					})
+				}),
 				new Cypress.Promise((resolve) => {
 					loadJS(jsLibrarySrc, () => {
 						const $script = document.querySelectorAll(`script.load-js-script[src="${jsLibrarySrc}"]`)
@@ -45,7 +59,7 @@ describe('load JS tests', () => {
 						resolve()
 					})
 				}),
-			])
+			]).then(reset)
 		})
 	})
 
@@ -75,30 +89,73 @@ describe('load JS tests', () => {
 					})
 				}),
 			])
+				.then(reset)
 		})
 	})
 
 	it('Expect to run callback immediately if the script has already been loaded', () => {
 		let calls = 0;
 		cy.visit(testHtmlFile).then(() => {
-			return Cypress.Promise.all([
-				new Cypress.Promise((resolve) => {
+			new Cypress.Promise((resolve) => {
+				loadJS(jsLibrarySrc, () => {
+					calls++
+					expect(calls).to.equal(1)
+					resolve()
+				})
+			}).then(() => {
+				return new Cypress.Promise((resolve) => {
 					loadJS(jsLibrarySrc, () => {
 						calls++
-						expect(calls).to.equal(1)
+						expect(calls).to.equal(2)
+						resolve()
+					})
+				})
+			}).then(reset)
+		})
+	})
+
+	it('Expect the loaded script global variables to be defined', () => {
+		cy.visit(testHtmlFile).then(() => {
+			return Cypress.Promise.all([
+				new Cypress.Promise((resolve) => {
+					expect(Window.moment).to.be.undefined
+					loadJS(jsLibrarySrc, () => {
+						// A crude method for marking the moment vs d3 calls
+						expect('moment').to.eq('moment')
+						expect(Window.moment).to.not.be.undefined
 						resolve()
 					})
 				}),
-				cy.wait(1000).then(() => {
-					new Cypress.Promise((resolve) => {
-						loadJS(jsLibrarySrc, () => {
-							calls++
-							expect(calls).to.equal(2)
-							resolve()
-						})
+				new Cypress.Promise((resolve) => {
+					expect(Window.d3).to.be.undefined
+					loadJS(altJsLibrarySrc, () => {
+						expect('d3').to.eq('d3')
+						expect(Window.d3).to.not.be.undefined
+						resolve()
 					})
 				})
-			])
+			]).then(reset)
+		})
+	})
+
+	it('Expect the *delayed* loaded script global variables to be defined', () => {
+		cy.visit(testHtmlFile).then(() => {
+			return new Cypress.Promise((resolve) => {
+				console.log(Window.moment)
+				expect(Window.moment).to.be.undefined
+				loadJS(jsLibrarySrc, () => {
+					expect(Window.moment).to.not.be.undefined
+					resolve()
+				})
+			}).then(() => {
+				return new Cypress.Promise((resolve) => {
+					expect(Window.d3).to.be.undefined
+					loadJS(altJsLibrarySrc, () => {
+						expect(Window.d3).to.not.be.undefined
+						resolve()
+					})
+				})
+			}).then(reset)
 		})
 	})
 })
