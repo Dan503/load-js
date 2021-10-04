62bba14b43520924f22bf2a1368578b09d6ea98f
Daniel Tonon <dan503.npm@gmail.com>
2021-10-04 21:29:49 +1100

    Making immediate callback test a little bit more robust

--- cypress/integration/immediateCallbackIfAlreadyLoaded.test.ts
+++ cypress/integration/immediateCallbackIfAlreadyLoaded.test.ts
@@ -1,9 +1,10 @@
 import loadJS from "../..";
-import { MomentJsLibrarySrc, testHtmlFile } from "../support/commonStuff";
+import { MomentJsLibrarySrc, testHtmlFile, Window } from "../support/commonStuff";
 
 it('Expect to run callback immediately if the script has already been loaded', () => {
 	let calls = 0;
 	cy.visit(testHtmlFile).then(() => {
+		expect(Window.moment).to.be.undefined
 		new Cypress.Promise((resolve) => {
 			loadJS(MomentJsLibrarySrc, () => {
 				calls++
@@ -11,12 +12,15 @@ it('Expect to run callback immediately if the script has already been loaded', (
 				resolve()
 			})
 		}).then(() => {
+			expect(Window.moment).to.not.be.undefined
 			return new Cypress.Promise((resolve) => {
 				loadJS(MomentJsLibrarySrc, () => {
 					calls++
 					expect(calls).to.equal(2)
 					resolve()
 				})
+				calls++
+				expect(calls).to.equal(3)
 			})
 		})
 	})
